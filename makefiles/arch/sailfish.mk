fb56b337e67e5b47dbe20dcf2a9347a2513cb858
Leif-Jöran Olsson <ljo@fripost.org>
2014-03-11 16:10:42 +0100

    Reenabling X-Nemo-Application-Type=silica-qt5 in .desktop file after addition of LDFLAGS += -pie in sailfish.mk.

--- makefiles/arch/sailfish.mk
+++ makefiles/arch/sailfish.mk
@@ -18,6 +18,7 @@ RM = rm -rvf
 CFLAGS += -DSAILFISH -DQT5
 CFLAGS += -DQT_DISABLE_DEPRECATED_BEFORE=4
 CFLAGS += -pipe -fno-exceptions -Wall -Wno-ctor-dtor-privacy -W -DLIBICONV_PLUG -fPIC
+LDFLAGS += -pie
 QTINCLUDE = -I/usr/include/qt5 -I/usr/include/qt5/QtCore -I/usr/include/qt5/QtQuick -I/usr/include/qt5/QtQml -I/usr/include/qt5/QtGui -I/usr/include/qt5/QtNetwork #-I/usr/include/qt5/QtScript
 UILIBS = -lQt5Core -lQt5Quick -lQt5Qml -lQt5Gui -lQt5Network -lsailfishapp
 
